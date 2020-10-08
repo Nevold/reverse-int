e38593e0ac2b388c7b7c4995bf504f1fbea6c09d
Nevold <nevold@tut.by>
2020-10-08 11:09:10 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n).split('').reverse().join('');
+    let strArr=str.split('');
+    console.log(strArr)
+    if(strArr[strArr.length-1]=='-'){
+     strArr.splice(strArr.length-1,1)
+    }
+    let num=+strArr.join('');
+   return num;
 }
